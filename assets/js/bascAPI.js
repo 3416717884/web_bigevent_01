26d3ecf8bc2f89c13f2f50cd0950c82df9f82bb8
sewr <3416717884@qq.com>
2020-11-26 10:36:21 +0800

    主页完成

--- assets/js/bascAPI.js
+++ assets/js/bascAPI.js
@@ -3,5 +3,21 @@ $(function () {
     // ajaxPrefilter 这个函数会在发送请求或者接受请求之前调用一下 可以获得里面的参数
     $.ajaxPrefilter(function (params) {
         params.url = bascURL + params.url
+        // 判断一下路径里面是否有/my如果有添加 headers
+        if (params.url.indexOf('/my/') != -1) {
+            params.headers = {
+                Authorization: localStorage.getItem('token') || ''
+            }
+        }
+        // 拦截所有响应 判断身份认证信息
+        params.complete = function (res) {
+            console.log(res);
+            if (res.responseJSON.status !== 0 && res.responseJSON.message === '身份认证失败！') {
+                // 清空本地存储
+                localStorage.removeItem('token')
+                // 跳转页面
+                location.href = "/login.html"
+            }
+        }
     })
 })
---
+++ assets/js/index.js
@@ -0,0 +1,51 @@
+$(function () {
+    // 获取用户信息
+    getUSerInof()
+
+    // 设置退出弹出框
+    $("#logout").on('click', function () {
+        // 弹出框
+        layer.confirm('确认退出登录?', { icon: 3, title: '提示' }, function (index) {
+            //do something
+            // 清空本地存储
+            localStorage.removeItem('token')
+            // 跳转登录页面
+            location.href = "/login.html"
+            // 取消模态框
+            layer.close(index);
+        });
+    })
+})
+// 获取用户信息 封装到入口函数外面
+// 原因 后面其他页面要调用 全局函数
+function getUSerInof() {
+    // 发送Ajax请求
+    $.ajax({
+        url: '/my/userinfo',
+        // headers: {
+        //     Authorization: localStorage.getItem('token') || ''
+        // },
+        success: function (res) {
+            console.log(res);
+            if (res.status !== 0) {
+                return layui.layer.msg(res.message)
+            }
+            renderAvatar(res.data)
+        }
+    })
+}
+function renderAvatar(user) {
+    // 获取到数据的 昵称
+    var name = user.nickname || user.username
+    // 渲染到页面上
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+    // 判断数据中是否有图片头像 如果没有就用文字头像
+    if (user.user_pic !== null) {
+        $('.layui-nav-img').attr('src', user.user_pic).show()
+        $('.text-avatar').hide()
+    } else {
+        $('.layui-nav-img').hide()
+        var text = name[0].toUpperCase()
+        $('.text-avatar').html(text)
+    }
+}
